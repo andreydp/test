e809c844a5d9a4ff54254bcfc12085ccddc93fb5
Andrey Poletaev <andrey.poletaev@inforeachinc.com>
2019-05-29 14:33:44 +0300

    AutoCommit Wed May 29 14:33:43 EEST 2019

--- src/GITCommit.java
+++ src/GITCommit.java
@@ -8,10 +8,7 @@ import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.transport.PushResult;
-import org.eclipse.jgit.transport.RemoteRefUpdate;
-import org.eclipse.jgit.transport.URIish;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.eclipse.jgit.transport.*;
 import org.eclipse.jgit.treewalk.AbstractTreeIterator;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 import org.eclipse.jgit.treewalk.FileTreeIterator;
@@ -22,24 +19,52 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 public class GITCommit {
+
+    public static boolean isCurrentBranchForward(Git git, String currentRevision) throws GitAPIException, IOException {
+        boolean result = false;
+
+        String remoteNewRevision = git.fetch().setDryRun(true).call().getAdvertisedRef("HEAD").getObjectId().getName();
+        String currentNewRevision = git.getRepository().resolve("master").getName();
+
+        // Check against remote repo, i.e. if there are new commits not fetched, dryRun = true
+        if (!remoteNewRevision.equals(currentRevision))
+        {
+            System.out.println("Remote has newer revision: " + remoteNewRevision);
+            result = true;
+        }
+
+        // Check current repo, i.e. if changes fetched and and not applied
+        else if (!currentNewRevision.equals(currentRevision))
+        {
+            System.out.println("Current repo has newer revision not merged " + remoteNewRevision);
+            result = true;
+        }
+        return result;
+    }
+
     public static void commitAllChanges(Repository repository, final String message) {
         try {
             final Git git = new Git(repository);
+            String currentRevision = git.getRepository().resolve("HEAD").getName();
+            if (isCurrentBranchForward(git, currentRevision)) {
+                System.out.println("There are undelivered changes. Won't commit. Exiting...");
+                System.exit(1);
+            }
             git.add().addFilepattern(".").call();
             final Status status = git.status().call();
             if (status.getConflicting().size() > 0) {
                 System.err.println("**** error: One or more conflicts are found. Manual intervention is required!");
             }
             if (status.getChanged().size() > 0 || status.getAdded().size() > 0 || status.getRemoved().size() > 0) {
-                System.out.println("Modified:");
+                if (status.getChanged().size() > 0) System.out.println("Modified:");
                 for (String s : status.getChanged()) {
                     System.out.println(s);
                 }
-                System.out.println("Added:");
+                if (status.getAdded().size() > 0)  System.out.println("Added:");
                 for (String s : status.getAdded()) {
                     System.out.println(s);
                 }
-                System.out.println("Deleted:");
+                if (status.getRemoved().size() > 0) System.out.println("Deleted:");
                 for (String s : status.getMissing()) {
                     System.out.println(s);
                 }
@@ -57,6 +82,7 @@ public class GITCommit {
 
     private static void pushToRemoteRepo(Repository localRepo, String httpUrl, String user, String password) throws GitAPIException, URISyntaxException, IOException {
         final Git git = new Git(localRepo);
+        String currentRevision = git.getRepository().resolve("HEAD").getName();
         RemoteAddCommand remoteAddCommand = git.remoteAdd();
         remoteAddCommand.setName("origin");
         remoteAddCommand.setUri(new URIish(httpUrl));
@@ -81,8 +107,8 @@ public class GITCommit {
             }
         }
         if (failed) {
-            System.out.println("Resetting working tree softly to origin/master...");
-            git.reset().setMode(ResetCommand.ResetType.SOFT).setRef("origin/master").call();
+            System.out.println("Resetting working tree softly to previous revision: " + currentRevision);
+            git.reset().setMode(ResetCommand.ResetType.SOFT).setRef(currentRevision).call();
         }
     }
 
@@ -106,6 +132,5 @@ public class GITCommit {
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 }
